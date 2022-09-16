9da8368800d6eaa2b9e1468f16b205544d8493e6
LSBYM1233 <113674358+LSBYM1233@users.noreply.github.com>
2022-09-16 17:48:11 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -57,7 +57,7 @@ export const config = {
   /**
    * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔
    */
-  USERS: ["oocpj6uHskjjo5b9g42zAQ-bGZLM","oocpj6pQgRoV4l69VrUY0bET3C8U"]
+  USERS: [
 
     {
       // 想要发送的人的名字
